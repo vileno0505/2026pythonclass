549e0506d17433e212866a06ae96a8dfe41f0eae
Jyun <latte0505@gmail.com>
2026-08-22 15:57:38 +0800

    update app

--- 0822/app.py
+++ 0822/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 home = st.Page('home.py', title='首頁')
 rate = st.Page('st-rate.py',title='取得匯率')
-stock = st.Page('st-rate.py',title='取得股價')
+stock = st.Page('st-stock.py',title='取得股價')
 weather = st.Page('st-weather.py',title='取得天氣')
 rss = st.Page('st-rss.py',title='RSS')
 
