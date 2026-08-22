2e4d6537c0b2435d50a37f4168b95e2d8491f0d5
Jyun <latte0505@gmail.com>
2026-08-22 16:31:59 +0800

    update home

--- 0822/home.py
+++ 0822/home.py
@@ -2,10 +2,7 @@ import streamlit as st
 
 st.title('首頁')
 
-st.title('HELLO WORLD')
-st.header('大標題')
-st.subheader('小標題')
-st.write('內文')
+st.header'HELLO WORLD')
 
 with open('0822/test.md', 'r', encoding='utf-8')as f:
     st.markdown(f.read())
